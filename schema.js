712810fd366164f68adb6263de62962d9bbb74ad
Raj Yadav <rahulyaxav72@gmail.com>
2025-08-02 14:13:31 +0530

    joi changed

--- schema.js
+++ schema.js
@@ -11,7 +11,7 @@ const listingSchema = Joi.object({
     price : Joi.number().required().min(0),
     location : Joi.string().required(),
     country :  Joi.string().required(),
-    category : Joi.array().required().min(1).max(3),
+    // category : Joi.array().required().min(1).max(3),
     }).required(),
     
 
